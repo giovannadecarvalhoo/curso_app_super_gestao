13036229c242148fc87ac249ceda813e4c2285e5
giovannadecarvalhoo <giovannadecarvalho2014@gmail.com>
2022-02-13 17:26:27 -0300

    refactor/adicionando as rotas, controllers, views, migrations, model

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'App\Http\Controllers\PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'App\Http\Controllers\SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
+Route::post('/contato', 'App\Http\Controllers\ContatoController@contato')->name('site.contato');
+Route::get('/login', function() { return 'Login'; })->name('site.site');
+
+
+Route::prefix('/app')->group(function() {
+    Route::get('/clientes', function() { return 'Clientes'; })->name('app.clientes');
+    Route::get('/fornecedores', 'FornecedorController@index')->name('app.fornecedores');
+    Route::get('/produtos', function() { return 'Clientes'; })->name('app.produtos');
+
 });
