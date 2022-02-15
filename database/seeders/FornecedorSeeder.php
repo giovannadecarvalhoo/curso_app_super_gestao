c0566545aefe416102c6ecd2855c0092cf596c02
giovannadecarvalhoo <giovannadecarvalho2014@gmail.com>
2022-02-15 18:02:10 -0300

    refactor/ajustes FornecedorSeeder

--- database/seeders/FornecedorSeeder.php
+++ database/seeders/FornecedorSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Fornecedor;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class FornecedorSeeder extends Seeder
 {
@@ -14,6 +16,28 @@ class FornecedorSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // instanciando o objeto
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        // o método create (atenção para o atributo fillable da classe)
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'contato@fornecedor200.com.br',
+        ]);
+
+        // insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'fornecedor300.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor300.com.br',
+        ]);
     }
 }
