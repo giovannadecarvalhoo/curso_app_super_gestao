9224b079194e6bfc0ff51e18e555089148c2f5c2
Giovanna de Carvalho <giovannadecarvalho2014@gmail.com>
2022-04-03 16:15:07 -0300

    refactor/add logAcessoModel e implementando LogAcessoMiddleware para todas as rotas

---
+++ app/Http/Middleware/LogAcessoMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Models\LogAcesso;
+
+class LogAcessoMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $ip = $request->server->get('REMOTE_ADDR');
+        $rota = $request->getRequestUri();
+        LogAcesso::create(['log' => "Ip $ip requisitou a rota $rota"]);
+        return $next($request);
+    }
+}
